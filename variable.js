2350f2d20b865595db31ecff27af96650c3d9c61
Vinicio <viniciojoj@gmail.com>
2021-11-19 19:11:48 -0600

    enums

--- variable.js
+++ variable.js
@@ -12,9 +12,23 @@ function doSomething2() {
 }
 doSomething();
 doSomething2();
+// primitive types
 var a;
 var b;
 var c;
 var d;
 var e = [1, 2, 3];
 var f = [1, true, 'e', false];
+// old js, whithout enums
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Blue;
--- variable.ts
+++ variable.ts
@@ -17,10 +17,27 @@ function doSomething2() {
 doSomething();
 doSomething2();
 
+// primitive types
 let a: number;
 let b: boolean;
 let c: string;
 let d: any;
 let e: number[] = [1, 2,3];
 let f: any[] = [1, true, 'e', false];
+
+// old js, whithout enums
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+// enums
+enum Color 
+{
+    Red, Green, Blue
+};
+
+let backgroundColor = Color.Blue;
+
+
+
 
